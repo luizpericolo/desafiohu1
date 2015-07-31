038bde8dd79905755eaf7e5a093561f35b3ed839
Luiz Pericolo <luizpericolo@gmail.com>
2015-07-30 23:00:38 -0300

    Adicionando um manager customizado para o model Disponibilidade.

--- widget_busca/busca/models/disponibilidade.py
+++ widget_busca/busca/models/disponibilidade.py
@@ -3,6 +3,10 @@
 from django.db import models
 from busca.models import BaseModel
 
+class DisponibilidadeManager(models.Manager):
+	def por_hotel_periodo(self, hotel, entrada, saida):
+		return self.filter(hotel=hotel, data__gte=entrada, data__lte=saida, disponivel=True)
+
 class Disponibilidade(BaseModel):
 	hotel = models.ForeignKey('busca.Hotel', verbose_name=u"Hotel")
 	data = models.DateField(verbose_name=u"Data")
@@ -13,3 +17,5 @@ class Disponibilidade(BaseModel):
 
 	def unicode(self):
 		return u"[Id]{} [Hotel]{} [Data]{} [Disponível]{}".format(self.id, self.hotel.nome, self.data.strftime("%d/%m/%Y"), "Sim" if self.disponivel else "Não")
+
+	objects = DisponibilidadeManager()

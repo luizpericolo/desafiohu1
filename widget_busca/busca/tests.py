aead17f483106c873b0e403d61105829cc7bb1f6
Luiz Pericolo <luizpericolo@gmail.com>
2015-07-31 19:46:23 -0300

    Escrevendo o resto dos testes de views.

--- widget_busca/busca/tests.py
+++ widget_busca/busca/tests.py
@@ -131,29 +131,143 @@ class BuscaTestCase(TestCase):
 		self.assertEquals(len(response_data), 0)
 
 	def test_busca_disponibilidade_cidade_existe_com_periodo(self):
-		pass
+		client = Client()
+
+		termo = 'itaborai'
+
+		post_params = {
+			'busca': termo,
+			'busca_type': 'cidade', # O tipo de busca é cidade.
+			'cidade_hotel_id': 78, # O id de itaboraí é 78.
+			'entrada': '03/05/2015',
+			'saida': '05/05/2015',
+		}
+
+		response = client.post(reverse('busca-disponibilidade'), post_params)
+
+		self.assertEquals(response.status_code, 200)
+
+		# Existem 12 hotéis com disponibilidade em itaboraí para o período 03/05/2015 - 05/05/2015.
+		self.assertEquals(len(response.context['hoteis']), 12)
 
 	def test_busca_disponibilidade_cidade_existe_sem_periodo(self):
-		pass
+		client = Client()
 
-	def test_busca_disponibilidade_cidade_nao_existe(self):
-		pass
+		termo = 'itaborai'
 
-	def test_busca_disponibiliade_hotel_existe_com_periodo(self):
-		pass
+		post_params = {
+			'busca': termo,
+			'busca_type': 'cidade', # O tipo de busca é cidade.
+			'cidade_hotel_id': 78, # O id de itaboraí é 78.
+			'periodo_indefinido': True, # Não estamos passando período para a busca.
+		}
+
+		response = client.post(reverse('busca-disponibilidade'), post_params)
+
+		self.assertEquals(response.status_code, 200)
+
+		# Existem 15 hotéis com disponibilidade na cidade itaborai.
+		self.assertEquals(len(response.context['hoteis']), 15)
+
+	def test_busca_disponibilidade_cidade_nao_existe_com_periodo(self):
+		client = Client()
+
+		termo = 'rio das ostras'
+
+		post_params = {
+			'busca': termo,
+			'busca_type': 'cidade', # O tipo de busca é cidade.
+			'cidade_hotel_id': 30, # O id de rio das ostras é 30.
+			'entrada': '01/08/2015',
+			'saida': '03/08/2015',
+		}
+
+		response = client.post(reverse('busca-disponibilidade'), post_params)
+
+		self.assertEquals(response.status_code, 200)
+
+		# Todas as disponibilidades listadas no banco são para o período 01/05/2015 - 30/05/2015.
+		self.assertEquals(len(response.context['hoteis']), 0)
 
 	def test_busca_disponibiliade_hotel_existe_sem_periodo(self):
-		pass
+		client = Client()
+
+		termo = 'casa de hilario'
+
+		post_params = {
+			'busca': termo,
+			'busca_type': 'hotel', # O tipo de busca é cidade.
+			'cidade_hotel_id': 78, # O id de itaboraí é 78.
+			'periodo_indefinido': True, # Não estamos passando período para a busca.
+		}
+
+		response = client.post(reverse('busca-disponibilidade'), post_params)
+
+		self.assertEquals(response.status_code, 200)
+
+		self.assertEquals(len(response.context['hoteis']), 1)
+
+	def test_busca_disponibiliade_hotel_existe_com_periodo(self):
+		client = Client()
+
+		termo = 'casa del embajador'
+
+		post_params = {
+			'busca': termo,
+			'busca_type': 'hotel', # O tipo de busca é hotel.
+			'cidade_hotel_id': 316, # O id de casa del embajador é 78.
+			'entrada': '01/05/2015',
+			'saida': '03/05/2015',
+		}
+
+		response = client.post(reverse('busca-disponibilidade'), post_params)
+
+		self.assertEquals(response.status_code, 200)
+
+		self.assertEquals(len(response.context['hoteis']), 1)
 
 	def test_detalhes_disponibilidade_hotel_existe_com_periodo(self):
-		pass
+		client = Client()
+
+		post_params = {
+			'hotel_id': 181, # O id de hotel casa lorenzo é 181.
+			'data_chegada': '01/05/2015',
+			'data_saida': '06/05/2015',
+		}
+
+		response = client.post(reverse('detalhes-disponibilidade'), post_params)
+
+		self.assertEquals(response.status_code, 200)
+
+		# Existe disponibilidade de dois dias para o hotel casa lorenzo no período 01/05/2015 - 06/05/2015
+		self.assertEquals(len(response.context['disponibilidades']), 2)
 
 	def test_detalhes_disponibilidade_hotel_existe_sem_periodo(self):
-		pass
+		client = Client()
+
+		post_params = {
+			'hotel_id': 7, # O id do hotel george sand é 7.
+		}
+
+		response = client.post(reverse('detalhes-disponibilidade'), post_params)
+
+		self.assertEquals(response.status_code, 200)
+
+		# Existe disponibilidade de 21 dias para o hotel george sand.
+		self.assertEquals(len(response.context['disponibilidades']), 21)
 
 	def test_detalhes_disponibilidade_hotel_nao_existe_com_periodo(self):
-		pass
+		client = Client()
 
-	# test_detalhes_disponibilidade_hotel_nao_existe_sem_periodo não faz sentido. Né?
+		post_params = {
+			'hotel_id': 6, # O id de hotel pousada são silmares é 6.
+			'data_chegada': '30/05/2015',
+			'data_saida': '31/05/2015',
+		}
 
-	# falta algum?
+		response = client.post(reverse('detalhes-disponibilidade'), post_params)
+
+		self.assertEquals(response.status_code, 200)
+
+		# Não existe disponibilidade para o hotel pousada são silmares no período 30/05/2015 - 31/05/2015
+		self.assertEquals(len(response.context['disponibilidades']), 0)

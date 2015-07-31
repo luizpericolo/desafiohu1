879eae21c2ebde37d97456ac79eff7c024bb5e9f
Luiz Pericolo <luizpericolo@gmail.com>
2015-07-30 23:04:09 -0300

    Adicionando um manager customizado para o model Hotel.

--- widget_busca/busca/models/hotel.py
+++ widget_busca/busca/models/hotel.py
@@ -3,12 +3,20 @@
 from django.db import models
 from busca.models import BaseModel
 
+class HotelManager(models.Manager):
+	def por_cidade(self, cidade):
+		u""" Retornando todos os hotéis de uma determinada cidade. """
+		return self.filter(cidade=cidade)
+
+
 class Hotel(BaseModel):
 	nome = models.CharField(max_length=255, verbose_name=u"Nome")
 	cidade = models.ForeignKey('busca.Cidade', verbose_name=u"Cidade")
 	
 	class Meta:
 		app_label = 'busca'
+
+	objects = HotelManager()
 
 	def unicode(self):
 		return u"[Id]{} [Nome]{} [Cidade]{}".format(self.id, self.nome, self.cidade.nome)
@@ -18,4 +26,3 @@ class Hotel(BaseModel):
 		hoteis = cls.objects.filter(nome__icontains=arg)
 		# typeahead_name é o nome da propriedade que será utlizada para popular o input com o nome do hotel quando ele for selecionado, sem incluir o nome da cidade.
 		return [{'id': hotel.id, 'typeahead_name': u'{}'.format(hotel.nome), 'name': u'{}, {}'.format(hotel.nome, hotel.cidade.nome), 'type': 'hotel'} for hotel in hoteis]
-

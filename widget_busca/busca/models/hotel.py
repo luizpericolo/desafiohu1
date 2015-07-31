785f191863b40279b973e7582ee3fe90e5a1989b
Luiz Pericolo <luizpericolo@gmail.com>
2015-07-30 23:43:16 -0300

    Criando função que diz se uma instância de hotel tem ou não disponibilidade num período desejado.

--- widget_busca/busca/models/hotel.py
+++ widget_busca/busca/models/hotel.py
@@ -8,7 +8,6 @@ class HotelManager(models.Manager):
 		u""" Retornando todos os hotéis de uma determinada cidade. """
 		return self.filter(cidade=cidade)
 
-
 class Hotel(BaseModel):
 	nome = models.CharField(max_length=255, verbose_name=u"Nome")
 	cidade = models.ForeignKey('busca.Cidade', verbose_name=u"Cidade")
@@ -26,3 +25,7 @@ class Hotel(BaseModel):
 		hoteis = cls.objects.filter(nome__icontains=arg)
 		# typeahead_name é o nome da propriedade que será utlizada para popular o input com o nome do hotel quando ele for selecionado, sem incluir o nome da cidade.
 		return [{'id': hotel.id, 'typeahead_name': u'{}'.format(hotel.nome), 'name': u'{}, {}'.format(hotel.nome, hotel.cidade.nome), 'type': 'hotel'} for hotel in hoteis]
+
+	def tem_disponibilidade_no_periodo(self, data_chegada, data_saida):
+		u""" Função que informa se o hotel tem disponibilidade no período desejado. """
+		return self.disponibilidade_set.filter(disponivel=True, data__gte=data_chegada, data__lte=data_saida).count() > 0

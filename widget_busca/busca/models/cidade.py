c0bf98e8bd6085b18333a28ae87960aa88a6f36d
Luiz Pericolo <luizpericolo@gmail.com>
2015-07-30 23:00:22 -0300

    Adicionando um manager customizado para o model Cidade.

--- widget_busca/busca/models/cidade.py
+++ widget_busca/busca/models/cidade.py
@@ -3,11 +3,19 @@
 from django.db import models
 from busca.models import BaseModel
 
+
+class CidadeManager(models.Manager):
+	def todos_hoteis_cidade(self, cidade):
+		return cidade.hotel_set.all()
+
+
 class Cidade(BaseModel):
 	nome = models.CharField(max_length=255, verbose_name=u"Nome")
 
 	class Meta:
 		app_label =	'busca'
+
+	objects = CidadeManager()
 
 	def unicode(self):
 		return u"[Id]{} [Nome]{}".format(self.id, self.nome)
@@ -17,6 +25,3 @@ class Cidade(BaseModel):
 		cidades = cls.objects.filter(nome__icontains=arg)
 		# typeahead_name é o nome da propriedade que será utlizada para popular o input com o nome da cidade quando ela for selecionada.
 		return [{'id': cidade.id, 'typeahead_name': u"{}".format(cidade.nome), 'name': u"{}".format(cidade.nome), 'type': 'cidade'} for cidade in cidades]
-
-
-
